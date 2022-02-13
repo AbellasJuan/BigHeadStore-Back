351249c2f3ed06950d4229d5d980546473834c7e
Adriano Fonseca <a.driano@outlook.com>
2022-02-13 15:32:21 -0300

    feat: added products route and controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,22 @@
+import { ObjectId } from 'mongodb';
 import db from '../database.js';
 
 export async function getProduct(req, res) {
-  const _id = req.body;
-
+  const _id = new ObjectId(req.params.id);
+  console.log(_id);
   try {
     const item = await db.collection("products").findOne({ _id });
-    res.send(item).status(200);
+    console.log(item);
+    res.send(item).status(201);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+}
+
+export async function getProducts(req, res) {
+  try {
+    const items = await db.collection("products").find().toArray();
+    return res.send(items).status(200);
   } catch (error) {
     return res.sendStatus(500);
   }
